84817424c00bb7a9e713e5ad5e4aff4453232911
Garrett Michael Flynn <garrettmflynn@gmail.com>
2025-08-17 11:05:35 -0700

    Add nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 export default {
     base: './',
-    // build: {
-    //     rollupOptions: {
-    //         input: {
-    //             main: 'index.html',
-    //             planner: 'planner.html',
-    //             solver: 'solver.html',
-    //             // complexSolver: 'complexSolver.html',
-    //         }
-    //     }
-    // }
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                planner: 'planner.html',
+                solver: 'solver.html',
+                // complexSolver: 'complexSolver.html',
+            }
+        }
+    }
 }
